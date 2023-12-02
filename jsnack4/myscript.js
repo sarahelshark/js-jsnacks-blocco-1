35a6f923b8c8a9704adad59391e362f90979971a
sarahelshark <sarahelsh183@gmail.com>
2023-12-02 11:36:14 +0100

    feat add 1st jjs commit+ some html content

---
+++ jsnack4/myscript.js
@@ -0,0 +1,16 @@
+/*
+In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+*/
+/*tools
+-const/let
+-query selector o getElementById
+-condizionale con booleano per verificare se presente in lista
+-manipolazione della dom per dare un eventuale messaggio di rientro o non rientro nella lista dei nomi 
+-array per creare una lista di confronto dei nomi invitati del programma 
+-event listener al bottone di submit del nome utente
+*/
+
+//creare lista di nomi per la verifica
+const partyList = ["Daisy", "Meyer", "Jordan", "Nick", "Tom"];
+//stampo per vedere se funziona
+console.log(partyList);
